8e7677be48790286a9f2ada975d3a559552287b7
livenleanaday <livenleanaday@gmail.com>
2025-06-13 19:07:08 +0700

    chore: update build.gradle, update libs, add spotless and replace glide with coil

--- app/src/main/kotlin/io/github/livenlearnaday/firebaseauth/ui/component/CustomImage.kt
+++ app/src/main/kotlin/io/github/livenlearnaday/firebaseauth/ui/component/CustomImage.kt
@@ -1,6 +1,5 @@
 package io.github.livenlearnaday.firebaseauth.ui.component
 
-import android.net.Uri
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -12,27 +11,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.integration.compose.placeholder
+import coil3.compose.AsyncImage
+import coil3.request.CachePolicy
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import coil3.svg.SvgDecoder
 import io.github.livenlearnaday.firebaseauth.R
 import io.github.livenlearnaday.firebaseauth.ui.theme.FirebaseauthTheme
 
-@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun CustomImage(
+    modifier: Modifier = Modifier,
     imageUrlString: String = "",
     placeHolderImageResource: Int,
-    modifier: Modifier = Modifier,
     borderColor: Color = Color.White
 ) {
 
     if (imageUrlString.isNotEmpty()) {
-        GlideImage(
+        AsyncImage(
             modifier = modifier
                 .clip(RoundedCornerShape(16.dp))
                 .border(
@@ -41,12 +42,15 @@ fun CustomImage(
                         width = 1.dp,
                         color = borderColor,
                     )
-
                 ),
-            model = Uri.parse(imageUrlString),
+            model = ImageRequest.Builder(LocalContext.current)
+                    .decoderFactory(SvgDecoder.Factory())
+                    .memoryCachePolicy(CachePolicy.ENABLED)
+                    .crossfade(true)
+                    .data( imageUrlString)
+                    .build(),
             contentDescription = stringResource(R.string.profile_image),
-            contentScale = ContentScale.Inside,
-            failure = placeholder(placeHolderImageResource)
+            fallback = painterResource(placeHolderImageResource)
         )
 
     } else {
--- app/src/main/kotlin/io/github/livenlearnaday/firebaseauth/util/DateUtil.kt
+++ app/src/main/kotlin/io/github/livenlearnaday/firebaseauth/util/DateUtil.kt
@@ -1,12 +1,14 @@
 package io.github.livenlearnaday.firebaseauth.util
 
 import timber.log.Timber
-import java.time.Instant
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 
 //time difference is +17353 , +17742 or about 17 seconds, added about 17.5s, firebase timestamp is faster than time from IDE
 fun Long.isWithinPast(minutes: Int): Boolean {
     Timber.d("log time from firebase server $this")
-    val nowInUTCMilliSeconds = Instant.now().toEpochMilli() + 17500
+    val now: Instant = Clock.System.now() // Current instant in UTC
+    val nowInUTCMilliSeconds = now.toEpochMilliseconds() + 17500
     Timber.d("log nowInUTCSeconds $nowInUTCMilliSeconds")
     val timeXMinBefore = nowInUTCMilliSeconds - (60 * minutes * 1000)
     Timber.d("log timeXMinBefore $timeXMinBefore")
